4065f67e36b9ff7e7830bc01666566b6499173f4
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-02-07 02:07:24 +0700

    COMPLETE: 700_search_in_a_binary_search_tree

---
+++ 700_search_in_a_binary_search_tree/700_search_in_a_binary_search_tree.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+/**
+ * My own solution
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *insert(TreeNode *root, int val)
+{
+    if (root == nullptr)
+        return new TreeNode(val);
+
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else if (val > root->val)
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+TreeNode *createRandomSortedBinaryTree(int n)
+{
+    TreeNode *root = nullptr;
+    for (int i = 0; i < n; ++i)
+    {
+        int randomValue = std::rand() % 100 + 1; // Generate random value between 1 and 100
+        root = insert(root, randomValue);
+    }
+    return root;
+}
+
+void printTree(TreeNode *root)
+{
+    if (root == nullptr)
+        return;
+
+    printTree(root->left);
+    std::cout << root->val << " ";
+    printTree(root->right);
+}
+
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (root == 0)
+        {
+            return 0;
+        }
+
+        if (val == root->val)
+        {
+            return root;
+        }
+        else if (val > root->val)
+        {
+            return searchBST(root->right, val);
+        }
+        else
+        {
+            return searchBST(root->left, val);
+        }
+    }
+};
+
+int main()
+{
+    std::srand(static_cast<unsigned>(std::time(nullptr))); // Seed the random number generator
+
+    Solution solution;
+    int n = 20; // Number of nodes
+    TreeNode *root = createRandomSortedBinaryTree(n);
+
+    std::cout << "Random Binary Tree with 20 nodes (In-order traversal):" << std::endl;
+    printTree(root);
+    std::cout << std::endl;
+
+    TreeNode *result = solution.searchBST(root, 25);
+    std::cout << "Result: " << std::endl;
+    printTree(result);
+    std::cout << std::endl;
+
+    return 0;
+}
