18c207503f3e29b3f8e5da1b6124f9e1d712e092
Duc Trung <trung.lyduc18@gmail.com>
2024-02-07 17:26:44 +0700

    ADD: change_name.py

---
+++ change_name.py
@@ -0,0 +1,8 @@
+INPUT_NAME = "16. 3Sum Closest"
+
+def main():
+    output = INPUT_NAME.lower().replace(" ", "_").replace(".", "") + ".py"
+    print(output)
+
+if __name__ == "__main__":
+    main()
