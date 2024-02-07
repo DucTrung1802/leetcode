934ad057c45813ffaa8014e4b4a122aa1403aec4
Duc Trung <trung.lyduc18@gmail.com>
2024-02-08 00:58:53 +0700

    UPGRADE: change_name.py

--- change_name.py
+++ change_name.py
@@ -1,8 +1,14 @@
-INPUT_NAME = "15. 3Sum"
+import os
+
+INPUT_NAME = "19. Remove Nth Node From End of List"
+
 
 def main():
-    output = INPUT_NAME.lower().replace(" ", "_").replace(".", "") + ".cpp"
-    print(output)
+    output = INPUT_NAME.lower().replace(" ", "_").replace(".", "")
+    os.makedirs(output)
+    os.chdir(output)
+    file = open(output + ".py", "a")
+
 
 if __name__ == "__main__":
     main()
