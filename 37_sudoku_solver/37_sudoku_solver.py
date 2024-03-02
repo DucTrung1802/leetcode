8dad4a2f4c01d0d4eb7acadf33f1b7e743db5292
Duc Trung <trung.lyduc18@gmail.com>
2024-03-03 00:13:16 +0700

    LEARNING: 37_sudoku_solver

--- 37_sudoku_solver/37_sudoku_solver.py
+++ 37_sudoku_solver/37_sudoku_solver.py
@@ -9,164 +9,94 @@ def print_sudoku(board: list[list[str]]) -> None:
 
 
 class Solution:
-    empty_mark = "."
-    selected_mark = "o"
-
     def solveSudoku(self, board: list[list[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
 
-        def isDuplicate(row: int, col: int, number: int) -> bool:
-            number = str(number)
-            return (
-                (number in horizontal_matrix[row])
-                or (number in vertical_matrix[col])
-                or (number in cube_matrix[int(row / 3) * 3 + int(col / 3)])
-            )
+        def rc2g(i, j):  # row,col to group idx
+            return i - i % 3 + j // 3
 
-        def addToBoard(row: int, col: int, value) -> None:
-            horizontal_matrix[row][col] = str(value)
+        def rm(i, j, x, masks):
+            mask = 1 << x
+            masks[0][i] |= mask
+            masks[1][j] |= mask
+            masks[2][rc2g(i, j)] |= mask
 
-            vertical_matrix[col][row] = str(value)
+        def GD():  # Greedy
+            nonlocal cur, board, rest
+            new = True
+            while new:
+                new = False
+                nxt = [[0] * 9 for i in range(3)]
+                for i in range(9):
+                    for j in range(9):
+                        if isinstance(board[i][j], int):
+                            maskr = cur[0][i]
+                            maskc = cur[1][j]
+                            maskg = cur[2][rc2g(i, j)]
+                            board[i][j] &= ~(maskr | maskc | maskg)
+                            if board[i][j] == 0:
+                                return False
+                            if board[i][j].bit_count() == 1:
+                                x = board[i][j].bit_length()
+                                board[i][j] = str(x)
+                                rm(i, j, x - 1, nxt)
+                                rm(i, j, x - 1, cur)
+                                rest -= 1
+                                new = True
+                cur = nxt
+            if rest:
+                return BF()
+            return True
 
-            cube_matrix_row = int(row / 3) * 3 + int(col / 3)
-            cube_matrix_col = (row % 3) * 3 + int(col % 3)
-            cube_matrix[cube_matrix_row][cube_matrix_col] = str(value)
+        def BF():  # Brute-Force
+            nonlocal cur, board, rest
+            for i in range(9):
+                for j in range(9):
+                    if isinstance(board[i][j], int):
+                        rest -= 1
+                        r = rest
+                        org = [row[:] for row in board]
+                        for x in range(board[i][j].bit_length()):
+                            if not (board[i][j] >> x) & 1:
+                                continue
+                            board[i][j] = str(x + 1)
+                            rm(i, j, x, cur)
+                            if GD() is True:
+                                return True
+                            for ii in range(9):
+                                for jj in range(9):
+                                    board[ii][jj] = org[ii][jj]
+                            cur = [[0] * 9 for i in range(3)]
+                            rest = r
+                        return False
 
-        def addEmptyMark(row: int, col: int):
-            addNumberToBoard(row, col, self.empty_mark)
-            board[row][col] = self.empty_mark
-
-        def addNumberToBoard(row: int, col: int, number) -> None:
-            addToBoard(row, col, number)
-            board[row][col] = self.selected_mark
-
-        def getCurrentNumber(row: int, col: int) -> int:
-            try:
-                return int(horizontal_matrix[row][col])
-            except ValueError:
-                return 0
-
-        def moveToNextNumber(row: int, col: int) -> tuple:
-            if col == 8:
-                col = 0
-                row += 1
-            else:
-                col += 1
-
-            travel_forward = True
-
-            return (row, col, travel_forward)
-
-        def moveToPreviousNumber(row: int, col: int) -> tuple:
-            if col == 0:
-                col = 8
-                row -= 1
-            else:
-                col -= 1
-
-            travel_forward = True
-
-            return (row, col, not travel_forward)
-
-        horizontal_matrix: list[list[str]] = copy.deepcopy(board)
-
-        vertical_matrix: list[list[str]] = [list(row) for row in zip(*board)]
-
-        cube_matrix: list[list[str]] = []
-
-        for i in range(0, len(board), 3):
-            for j in range(0, len(board[0]), 3):
-                cube_row = []
-                for row in board[i : i + 3]:
-                    cube_row.extend(row[j : j + 3])
-                cube_matrix.append(cube_row)
-
-        row: int = 0
-        col: int = 0
-        travel_forward: bool = True
-
-        # Coordinates of the first empty value
-        start_row_empty_value = -1
-        start_col_empty_value = -1
-
-        while 0 <= row <= 8 and 0 <= col <= 8:
-            # Fixed number
-            if (
-                board[row][col] != self.empty_mark
-                and board[row][col] != self.selected_mark
-            ):
-                if not travel_forward and not (
-                    row == start_row_empty_value and col == start_col_empty_value
-                ):
-                    row, col, travel_forward = moveToPreviousNumber(row, col)
+        rest = 81
+        # 1x9 row, 9x1 col, 3x3 group
+        cur = [[0] * 9 for i in range(3)]
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    # bit: 111111111
+                    board[i][j] = 511
                 else:
-                    row, col, travel_forward = moveToNextNumber(row, col)
-
-                continue
-
-            # Empty mark
-            if board[row][col] == self.empty_mark:
-                if start_row_empty_value < 0:
-                    start_row_empty_value = row
-                    start_col_empty_value = col
-
-                for i in range(1, 10):
-                    if isDuplicate(row, col, i):
-                        # i reaches max value and still duplicate -> we need to get back a step and change previous value
-                        if i == 9:
-                            addEmptyMark(row, col)
-                            row, col, travel_forward = moveToPreviousNumber(row, col)
-                            break
-                    else:
-                        addNumberToBoard(row, col, i)
-                        row, col, travel_forward = moveToNextNumber(row, col)
-                        break
-
-            # Selected mark
-            elif board[row][col] == self.selected_mark:
-                current_value = getCurrentNumber(row, col) + 1
-
-                # Sill available to test this current value
-                while 1 <= current_value <= 9:
-                    if not isDuplicate(row, col, current_value):
-                        addNumberToBoard(row, col, current_value)
-                        break
-                    current_value += 1
-
-                # Get back a step
-                else:
-                    addEmptyMark(row, col)
-                    row, col, travel_forward = moveToPreviousNumber(row, col)
-                    continue
-
-                # Select next value
-                row, col, travel_forward = moveToNextNumber(row, col)
-
-        for i in range(0, 9):
-            for j in range(0, 9):
-                board[i][j] = horizontal_matrix[i][j]
-
-        # print_sudoku(board)
-        # print()
-        # print_sudoku(horizontal_matrix)
-        # print()
-        # print_sudoku(vertical_matrix)
-        # print()
-        # print_sudoku(cube_matrix)
+                    x = int(board[i][j]) - 1
+                    rm(i, j, x, cur)
+                    rest -= 1
+        GD()
 
 
 sudoku_board = [
-    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-    [".", "9", "8", ".", ".", ".", ".", "6", "."],
-    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-    [".", "6", ".", ".", ".", ".", "2", "8", "."],
-    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+    [".", "7", ".", ".", ".", "5", ".", ".", "."],
+    ["1", ".", ".", ".", "3", ".", "5", ".", "8"],
+    [".", ".", ".", "2", ".", "9", ".", "6", "."],
+    ["9", "1", ".", "5", ".", ".", "4", "2", "."],
+    ["6", "8", ".", "3", ".", ".", ".", "1", "."],
+    ["2", "5", "4", ".", "9", ".", ".", ".", "3"],
+    ["7", ".", "6", "8", ".", "1", ".", "4", "."],
+    ["3", "4", "5", ".", ".", "6", ".", "7", "1"],
+    [".", ".", "1", ".", "7", ".", "2", ".", "6"],
 ]
 
 print_sudoku(sudoku_board)
