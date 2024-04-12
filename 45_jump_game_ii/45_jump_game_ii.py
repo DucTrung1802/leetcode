80a0e44d889a60dc0eaed0f1f81a2edb07af04d8
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-04-13 01:31:06 +0700

    COMPLETE: 45_jump_game_ii - my solution

--- 45_jump_game_ii/45_jump_game_ii.py
+++ 45_jump_game_ii/45_jump_game_ii.py
@@ -1,24 +1,46 @@
 class Solution:
     def jump(self, nums: list[int]) -> int:
+        if len(nums) == 1:
+            return 0
+
         list_step: list[int] = []
 
         current_index = len(nums) - 1
         while current_index >= 0:
-            for back_step in range(1, current_index + 1):
-                if nums[current_index - back_step] >= back_step:
-                    if len(list_step) == 0:
-                        list_step = [current_index] + list_step
-                    else:
-                        can_step_to = current_index + nums[current_index]
-                        for i in range(0, len(list_step)):
-                            if can_step_to > list_step[i]:
-                                list_step = list_step[1:]
-                            else:
-                                list_step = [current_index] + list_step
+            if current_index == 0:
+                can_step_to = current_index + nums[current_index]
+                skip_step = 0
+                for i in range(1, len(list_step)):
+                    if can_step_to < list_step[i]:
+                        break
+                    skip_step += 1
+                list_step = list_step[skip_step:]
+                list_step = [current_index] + list_step
 
-                current_index = current_index - back_step
+            else:
+                for back_step in range(1, current_index + 1):
+                    if nums[current_index - back_step] >= back_step:
+                        if len(list_step) == 0:
+                            list_step = [current_index] + list_step
+                        else:
+                            can_step_to = current_index + nums[current_index]
+                            skip_step = 0
+                            for i in range(1, len(list_step)):
+                                if can_step_to < list_step[i]:
+                                    break
+                                skip_step += 1
+                            list_step = list_step[skip_step:]
+
+                            list_step = [current_index] + list_step
+
+                        break
+
+            current_index -= back_step
+
+        return len(list_step) - 1
 
 
-# [2, 1, 3, 0, 3, 0, 4, 0, 5, 5, 3, 2, 3, 5, 4, 4, 2, 2, 1, 1]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+# [2, 1, 3, 0, 3, 0, 4, 0, 5, 5,  3,  2,  3,  5,  4,  4,  2,  2,  1,  1]
 hello = Solution()
-hello.jump([2, 1, 3, 0, 3, 0, 4, 0, 5, 5, 3, 2, 3, 5, 4, 4, 2, 2, 1, 1])
+print(hello.jump([4, 2, 1, 1]))
