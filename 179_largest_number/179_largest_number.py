b165691e44e4a9fda3d156cded85cd67b0f17ce0
Ly Duc Trung <trung.lyduc18@gmail.com>
2024-04-13 04:32:54 +0700

    WOKRING: fix if the input is as written

--- 179_largest_number/179_largest_number.py
+++ 179_largest_number/179_largest_number.py
@@ -1,26 +1,53 @@
+import math
+
+
 class Solution:
     def largestNumber(self, nums: list[int]) -> str:
         if len(nums) == 1:
             return str(nums[0])
 
-        max_length = len(str(max(nums)))
         str_list = list(map(str, nums))
         result = ""
 
-        for order in range(1, max_length + 1):
-            current_best = ""
-            for index in range(0, len(str_list)):
-                if len(str_list[index]) >= order:
-                    if not current_best:
+        while len(str_list) > 0:
+            current_best = str_list[0]
+            for index in range(1, len(str_list)):
+
+                # Length is equal, compare magnitude
+                if (
+                    len(str_list[index]) == len(current_best)
+                    and str_list[index] > current_best
+                ):
+                    current_best = str_list[index]
+
+                # If length is smaller, then multiply the smaller one
+                if len(str_list[index]) < len(current_best):
+                    length_diff = len(current_best) - len(str_list[index])
+                    extend_str_list_index = (
+                        str_list[index]
+                        + (str_list[index] * (length_diff // len(str_list[index]) + 1))[
+                            :length_diff
+                        ]
+                    )
+                    if extend_str_list_index > current_best:
                         current_best = str_list[index]
-                        continue
-                    else:
-                        # Same length
-                        if len(current_best) == len(str_list[index]):
-                            current_best = "max"
 
-                        # Different length
+                elif len(current_best) < len(str_list[index]):
+                    length_diff = len(str_list[index]) - len(current_best)
+                    extend_current_best = (
+                        current_best
+                        + (current_best * (length_diff // len(current_best) + 1))[
+                            :length_diff
+                        ]
+                    )
+                    if str_list[index] > extend_current_best:
+                        current_best = str_list[index]
+
+            result += current_best
+            str_list.remove(current_best)
+
+        return result
 
 
-# hello = Solution()
-# hello.largestNumber([3, 30, 34, 5, 9])
+hello = Solution()
+print(hello.largestNumber([432, 43243]))
