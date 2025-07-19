1ba4ac85e2c38a4aae16a267f6ab9ffd0b761792
DucTrung1802 <trung.lyduc18@gmail.com>
2025-07-19 18:12:26 +0700

    UDPATE: change name code

--- change_name.py
+++ change_name.py
@@ -1,13 +1,14 @@
 import os
 
-INPUT_NAME = "19. Remove Nth Node From End of List"
+INPUT_NAME = "1757. Recyclable and Low Fat Products"
+TYPE = "sql"
 
 
 def main():
     output = INPUT_NAME.lower().replace(" ", "_").replace(".", "")
     os.makedirs(output)
     os.chdir(output)
-    file = open(output + ".py", "a")
+    file = open(output + f".{TYPE.lower()}", "a")
 
 
 if __name__ == "__main__":
