10a902b8d04673b546e71c680f50b6278dcde6c4
DucTrung1802 <trung.lyduc18@gmail.com>
2026-02-24 16:44:02 +0700

    REFACTOR: structure of repo

--- change_name.py
+++ change_name.py
@@ -1,14 +1,28 @@
 import os
+from enum import Enum
 
-INPUT_NAME = "1757. Recyclable and Low Fat Products"
-TYPE = "sql"
+
+class LEETCODE_TOPIC(Enum):
+    ALGORITHMS = "algorithms"
+    DATABASE = "database"
+    SHELL = "shell"
+    CONCURRENCY = "concurrency"
+    JAVASCRIPT = "javascript"
+    PANDAS = "pandas"
+
+
+name = "217. Contains Duplicate"
+topic = LEETCODE_TOPIC.ALGORITHMS
+extension = "py"
 
 
 def main():
-    output = INPUT_NAME.lower().replace(" ", "_").replace(".", "")
-    os.makedirs(output)
-    os.chdir(output)
-    file = open(output + f".{TYPE.lower()}", "a")
+    folder_name = topic.value
+    file_name = name.lower().replace(" ", "_").replace(".", "")
+    output_folder = f"{folder_name}/{file_name}"
+    output_file = f"{output_folder}/{file_name}.{extension}"
+    os.makedirs(output_folder, exist_ok=True)
+    file = open(output_file, "a")
 
 
 if __name__ == "__main__":
